c4bd123c1356e6446a471747ff5337a84a021833
Thibaut Durand <durand.tibo@gmail.com>
2025-11-21 23:50:16 -0800

    Add `reduction` module

---
+++ src/sonnix/functional/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain functional implementation of some modules."""
+
+from __future__ import annotations
+
+__all__ = ["check_loss_reduction_strategy", "reduce_loss"]
+
+from sonnix.functional.reduction import check_loss_reduction_strategy, reduce_loss
