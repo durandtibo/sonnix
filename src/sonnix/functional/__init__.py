324ae2e9384a6c40a72804a74ddf37c5e311aa05
Thibaut Durand <durand.tibo@gmail.com>
2025-11-22 11:57:53 -0800

    Add `error` module

--- src/sonnix/functional/__init__.py
+++ src/sonnix/functional/__init__.py
@@ -2,6 +2,17 @@ r"""Contain functional implementation of some modules."""
 
 from __future__ import annotations
 
-__all__ = ["check_loss_reduction_strategy", "reduce_loss"]
+__all__ = [
+    "absolute_error",
+    "absolute_relative_error",
+    "check_loss_reduction_strategy",
+    "reduce_loss",
+    "symmetric_absolute_relative_error",
+]
 
+from sonnix.functional.error import (
+    absolute_error,
+    absolute_relative_error,
+    symmetric_absolute_relative_error,
+)
 from sonnix.functional.reduction import check_loss_reduction_strategy, reduce_loss
