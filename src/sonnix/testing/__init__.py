6993743fdbfbb22c06b4f74456bf7ab379ccb49d
Thibaut Durand <durand.tibo@gmail.com>
2025-11-27 08:28:57 -0800

    Update docs

--- src/sonnix/testing/__init__.py
+++ src/sonnix/testing/__init__.py
@@ -2,10 +2,11 @@ r"""Define some utility functions for testing."""
 
 from __future__ import annotations
 
-__all__ = ["cuda_available", "objectory_available", "objectory_not_available"]
+__all__ = ["cuda_available", "cuda_not_available", "objectory_available", "objectory_not_available"]
 
 from sonnix.testing.fixtures import (
     cuda_available,
+    cuda_not_available,
     objectory_available,
     objectory_not_available,
 )
--- src/sonnix/testing/fixtures.py
+++ src/sonnix/testing/fixtures.py
@@ -2,7 +2,7 @@ r"""Define some PyTest fixtures."""
 
 from __future__ import annotations
 
-__all__ = ["cuda_available", "objectory_available", "objectory_not_available"]
+__all__ = ["cuda_available", "cuda_not_available", "objectory_available", "objectory_not_available"]
 
 import pytest
 import torch
@@ -12,7 +12,9 @@ from sonnix.utils.imports import is_objectory_available
 cuda_available = pytest.mark.skipif(
     not torch.cuda.is_available(), reason="Requires a device with CUDA"
 )
-
+cuda_not_available = pytest.mark.skipif(
+    torch.cuda.is_available(), reason="Skip if CUDA is available"
+)
 
 objectory_available = pytest.mark.skipif(not is_objectory_available(), reason="Require objectory")
 objectory_not_available = pytest.mark.skipif(
