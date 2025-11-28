c6d7309fe9212050439145cf9dd2a50aeaaef472
Thibaut Durand <durand.tibo@gmail.com>
2025-11-27 19:48:54 -0800

    Add integration tests for `DynamicTanh`

--- tests/integration/modules/activations/test_dyt.py
+++ tests/integration/modules/activations/test_dyt.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 import torch
 from torch.nn import MSELoss
 
-from sonnix.modules import ScaleAndShift
+from sonnix.modules import DynamicTanh
 from sonnix.utils.loss import is_loss_decreasing_with_adam
 
 
-def test_scale_and_shift_loss() -> None:
+def test_dynamic_tanh_loss() -> None:
     assert is_loss_decreasing_with_adam(
-        module=ScaleAndShift(normalized_shape=6),
+        module=DynamicTanh(normalized_shape=6),
         criterion=MSELoss(),
         feature=torch.randn(8, 6),
         target=torch.randn(8, 6),
