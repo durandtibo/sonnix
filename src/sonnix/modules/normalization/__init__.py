255d5c2d22cccda2b2c1cbcb6662f59b29843c38
Thibaut Durand <durand.tibo@gmail.com>
2025-11-27 20:16:59 -0800

    Move `DynamicSinh/Tanh` to `normalization` (#54)

---
+++ src/sonnix/modules/normalization/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain normalization modules."""
+
+from __future__ import annotations
+
+__all__ = ["DynamicSinh", "DynamicTanh"]
+
+from sonnix.modules.normalization.dynamic import DynamicSinh, DynamicTanh
