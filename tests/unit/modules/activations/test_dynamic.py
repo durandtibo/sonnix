f93cea2aff330ee9845bc8df42e70383c756c67a
Thibaut Durand <durand.tibo@gmail.com>
2025-11-27 20:09:59 -0800

    Add `DynamicSinh` (#53)

--- tests/unit/modules/activations/test_dynamic.py
+++ tests/unit/modules/activations/test_dynamic.py
@@ -5,9 +5,174 @@ import torch
 from coola import objects_are_allclose
 from coola.utils.tensor import get_available_devices
 
-from sonnix.modules import DynamicTanh
+from sonnix.modules import DynamicSinh, DynamicTanh
 
 SIZES = (1, 2, 3)
+
+
+#################################
+#     Tests for DynamicSinh     #
+#################################
+
+
+def test_dynamic_sinh_str() -> None:
+    assert str(DynamicSinh(normalized_shape=5)) == "DynamicSinh(normalized_shape=(5,))"
+
+
+@pytest.mark.parametrize(("shape_input", "expected"), [(5, (5,)), ([3], (3,)), ((4, 2), (4, 2))])
+def test_dynamic_sinh_normalized_shape(
+    shape_input: int | list[int] | tuple[int, ...], expected: tuple[int, ...]
+) -> None:
+    module = DynamicSinh(shape_input)
+    assert module._normalized_shape == expected
+    assert module.weight.shape == expected
+    assert module.bias.shape == expected
+
+
+def test_dynamic_sinh_normalized_shape_int() -> None:
+    module = DynamicSinh(normalized_shape=5)
+    assert module.weight.equal(torch.ones(5))
+    assert module.bias.equal(torch.zeros(5))
+
+
+def test_dynamic_sinh_normalized_shape_list() -> None:
+    module = DynamicSinh(normalized_shape=[5])
+    assert module.weight.equal(torch.ones(5))
+    assert module.bias.equal(torch.zeros(5))
+
+
+def test_dynamic_sinh_normalized_shape_tuple_1() -> None:
+    module = DynamicSinh(normalized_shape=(5,))
+    assert module.weight.equal(torch.ones(5))
+    assert module.bias.equal(torch.zeros(5))
+
+
+def test_dynamic_sinh_normalized_shape_tuple_2() -> None:
+    module = DynamicSinh(normalized_shape=(4, 5))
+    assert module.weight.equal(torch.ones(4, 5))
+    assert module.bias.equal(torch.zeros(4, 5))
+
+
+def test_dynamic_sinh_alpha_default() -> None:
+    module = DynamicSinh(normalized_shape=[5])
+    assert module.alpha.equal(torch.tensor([0.5]))
+
+
+def test_dynamic_sinh_alpha_2() -> None:
+    module = DynamicSinh(normalized_shape=[5], alpha_init_value=2.0)
+    assert module.alpha.equal(torch.tensor([2.0]))
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("batch_size", SIZES)
+@pytest.mark.parametrize("dim", SIZES)
+@pytest.mark.parametrize("mode", [True, False])
+def test_dynamic_sinh_forward_2d(device: str, batch_size: int, dim: int, mode: bool) -> None:
+    device = torch.device(device)
+    module = DynamicSinh(normalized_shape=dim).to(device=device)
+    module.train(mode)
+    out = module(torch.rand(batch_size, dim, device=device))
+    assert out.shape == (batch_size, dim)
+    assert out.device == device
+    assert out.dtype == torch.float
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("batch_size", SIZES)
+@pytest.mark.parametrize("dims", [(5,), (2, 3), (3, 3), (2, 3, 4)])
+@pytest.mark.parametrize("mode", [True, False])
+def test_dynamic_sinh_forward_2d_plus(
+    device: str, batch_size: int, dims: tuple[int, ...], mode: bool
+) -> None:
+    device = torch.device(device)
+    module = DynamicSinh(normalized_shape=dims).to(device=device)
+    module.train(mode)
+    out = module(torch.rand(batch_size, *dims, device=device))
+    assert out.shape == (batch_size, *dims)
+    assert out.device == device
+    assert out.dtype == torch.float
+
+
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("batch_size", SIZES)
+@pytest.mark.parametrize("seq_len", SIZES)
+@pytest.mark.parametrize("dim", SIZES)
+@pytest.mark.parametrize("mode", [True, False])
+def test_dynamic_sinh_forward_3d_seq(
+    device: str, batch_size: int, seq_len: int, dim: int, mode: bool
+) -> None:
+    device = torch.device(device)
+    module = DynamicSinh(normalized_shape=dim).to(device=device)
+    module.train(mode)
+    out = module(torch.rand(batch_size, seq_len, dim, device=device))
+    assert out.shape == (batch_size, seq_len, dim)
+    assert out.device == device
+    assert out.dtype == torch.float
+
+
+def test_dynamic_sinh_forward_2d_init_weights() -> None:
+    module = DynamicSinh(normalized_shape=5)
+    assert objects_are_allclose(
+        module(torch.tensor([[-2.0, -1.0, 0.0, 1.0, 2.0], [3.0, 2.0, 1.0, 2.0, 3.0]])),
+        torch.tensor(
+            [
+                [
+                    -1.175201177597046,
+                    -0.5210952758789062,
+                    0.0,
+                    0.5210952758789062,
+                    1.175201177597046,
+                ],
+                [
+                    2.129279375076294,
+                    1.175201177597046,
+                    0.5210952758789062,
+                    1.175201177597046,
+                    2.129279375076294,
+                ],
+            ]
+        ),
+    )
+
+
+def test_dynamic_sinh_forward_2d_custom_weights() -> None:
+    module = DynamicSinh(normalized_shape=5)
+    with torch.no_grad():
+        module.weight.data += 1.0
+        module.bias.data += 1.0
+    assert objects_are_allclose(
+        module(torch.tensor([[-2.0, -1.0, 0.0, 1.0, 2.0], [3.0, 2.0, 1.0, 2.0, 3.0]])),
+        torch.tensor(
+            [
+                [
+                    -1.3504023551940918,
+                    -0.0421905517578125,
+                    1.0,
+                    2.0421905517578125,
+                    3.350402355194092,
+                ],
+                [
+                    5.258558750152588,
+                    3.350402355194092,
+                    2.0421905517578125,
+                    3.350402355194092,
+                    5.258558750152588,
+                ],
+            ]
+        ),
+    )
+
+
+def test_dynamic_sinh_reset_parameters() -> None:
+    module = DynamicSinh(normalized_shape=5)
+    with torch.no_grad():
+        module.alpha.data += 1.0
+        module.weight.data += 1.0
+        module.bias.data += 1.0
+    module.reset_parameters()
+    assert module.alpha.equal(torch.tensor([0.5]))
+    assert module.weight.equal(torch.ones(5))
+    assert module.bias.equal(torch.zeros(5))
 
 
 #################################
