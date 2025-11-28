4afcc1b604ce5c6c1764bf2c852acef8d5f2b69c
Thibaut Durand <durand.tibo@gmail.com>
2025-11-28 13:22:25 -0800

    Add `rectifier_asinh_unit` (#64)

--- src/sonnix/functional/activation/__init__.py
+++ src/sonnix/functional/activation/__init__.py
@@ -2,6 +2,7 @@ r"""Contain functional implementation of some activation layers."""
 
 from __future__ import annotations
 
-__all__ = ["safe_exp", "safe_log"]
+__all__ = ["rectifier_asinh_unit", "safe_exp", "safe_log"]
 
+from sonnix.functional.activation.hyperbolic import rectifier_asinh_unit
 from sonnix.functional.activation.math import safe_exp, safe_log
