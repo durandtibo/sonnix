b6e6ae288a17fb47ea3a7d2334f9813304543387
Thibaut Durand <durand.tibo@gmail.com>
2025-11-29 00:36:36 -0800

    Moving dynamic activations

--- src/sonnix/modules/normalization/__init__.py
+++
@@ -1,7 +0,0 @@
-r"""Contain normalization modules."""
-
-from __future__ import annotations
-
-__all__ = ["DynamicAsinh", "DynamicTanh"]
-
-from sonnix.modules.normalization.dynamic import DynamicAsinh, DynamicTanh
