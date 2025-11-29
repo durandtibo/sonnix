6e203ae93571ac8aa6fdd2a4b216c96879a3707a
Thibaut Durand <durand.tibo@gmail.com>
2025-11-29 12:25:15 -0800

    Fix unit tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,10 +2,14 @@ r"""Define some utility functions/classes."""
 
 from __future__ import annotations
 
-__all__ = ["ExamplePair"]
+__all__ = ["DEVICES_WITHOUT_MPS", "ExamplePair"]
 
 from dataclasses import dataclass
 from typing import Any
+
+from coola.utils.tensor import get_available_devices
+
+DEVICES_WITHOUT_MPS = (d for d in get_available_devices() if not d.startswith("mps:"))
 
 
 @dataclass
