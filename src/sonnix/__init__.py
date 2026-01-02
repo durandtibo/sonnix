ab05578ed82b599d10f9752fb1cf7ef28409866d
Thibaut Durand <durand.tibo@gmail.com>
2026-01-02 07:05:53 -0800

    Add `__version__`

--- src/sonnix/__init__.py
+++ src/sonnix/__init__.py
@@ -3,3 +3,15 @@ r"""The sonnix package - A library of PyTorch modules.
 Sonnix provides a collection of custom PyTorch modules, layers, and loss
 functions for building deep learning models.
 """
+
+from __future__ import annotations
+
+__all__ = ["__version__"]
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    # Package is not installed, fallback if needed
+    __version__ = "0.0.0"
